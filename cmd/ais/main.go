59be0e4835bd26c4a6c07aebee635780f4eda83a
esverde <esverde@foxmail.com>
2026-08-31 13:31:58 +0800

    🔖 chore(release): 版本号提升至 0.1.1

--- cmd/ais/main.go
+++ cmd/ais/main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/esverde/ais/internal/ui"
 )
 
-const version = "0.1.0"
+const version = "0.1.1"
 
 func main() {
 	configOverride := flag.String("config", "", "configuration file path")
